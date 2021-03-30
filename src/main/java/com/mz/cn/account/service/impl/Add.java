2a976e4dfb52a441f4c7876d24adce5674146120
huanlin <HL941201>
2021-03-30 16:24:39 +0800

    增加aAdd

---
+++ src/main/java/com/mz/cn/account/service/impl/Add.java
@@ -0,0 +1,13 @@
+package com.mz.cn.account.service.impl;
+
+/**
+ * @Description <p></p>
+ * @Author: Huanglin
+ * @Date: 2021/3/30 16:22
+ * @Version 0.0.1
+ */
+public class Add {
+    public void add(){
+        System.out.println("这是一个测文件");
+    }
+}
